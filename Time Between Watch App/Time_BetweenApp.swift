d6be70cbda774bb85633920fd16fe0c167f5b81c
Barry Dunn <barrdunn@me.com>
2023-07-09 14:07:22 -0500

    Initial Commit

---
+++ Time Between Watch App/Time_BetweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_BetweenApp.swift
+//  Time Between Watch App
+//
+//  Created by Barry Dunn on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Time_Between_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
